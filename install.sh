e6e6c456bd9afe2b0ddb5273b02f6798577bf95b
Lucas <96977171+lucasenlucas@users.noreply.github.com>
2025-04-25 19:48:13 +0200

    Delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "📥 LucasKit wordt geïnstalleerd..."
-
-# Kopieer het script naar /usr/local/bin en maak het uitvoerbaar
-sudo cp lucaskit.py /usr/local/bin/lucaskit
-sudo chmod +x /usr/local/bin/lucaskit
-
-echo "✅ Installatie voltooid! Je kunt de tool starten met het command: lucaskit"
